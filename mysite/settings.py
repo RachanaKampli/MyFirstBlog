5ea1ac2665fec72b8f5cb2c5c30e313a2c74e4d5
rachanakampli <rachanakampli@gmail.com>
2021-10-05 12:35:25 +0530

    modified html files.

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 # Application definition
 
 INSTALLED_APPS = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
